cf5577710ebf7c5a66a34c28da0406c4daf052d8
AQ <cshazemelwazery@gmail.com>
2024-11-30 15:21:58 +0200

    added favorite,watch later , download

---
+++ lib/features/menu_/presentation/screens/favorite.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/core/utils/colors/app_colors.dart';
+import 'package:movie_app/features/App/costume_widgets/costume_app_bar/costume_app_bar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../../core/utils/assets/app_images.dart';
+import '../../../../core/utils/styles/app_text_style.dart';
+import '../widgets/favorite_widget/custom_list_view_favorite.dart';
+
+class FavoritePage extends StatelessWidget {
+  final List<Map<String, dynamic>> movies = [
+    {
+      'title': 'Avengers: Endgame',
+      'image': AppImages.movieBackground,
+      'year': 2019
+    },
+    {'title': 'Soul', 'image': AppImages.medium, 'year': 2020},
+    {'title': 'Mulan', 'image': AppImages.movieBackground, 'year': 2020},
+    {'title': 'Knives Out', 'image': AppImages.movieBackground, 'year': 2019},
+  ];
+
+  FavoritePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          CostumeAppBar(
+              title: AppLocalizations.of(context)!.favorite, isBack: true),
+          Expanded(
+              child: SingleChildScrollView(
+                  child: CustomListViewFavorite(
+            movies: movies,
+          )))
+        ],
+      ),
+    );
+  }
+}
+
+
---
+++ lib/features/menu_/presentation/widgets/watch_later_widget/watch_later_horizontal_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/features/menu_/presentation/widgets/watch_later_widget/watch_later_custom_card.dart';
+
+import '../../../../../core/utils/assets/app_images.dart';
+
+class WatchLaterHorizontalView extends StatefulWidget {
+  const WatchLaterHorizontalView({super.key});
+
+  @override
+  _WatchLaterHorizontalViewState createState() => _WatchLaterHorizontalViewState();
+}
+
+class _WatchLaterHorizontalViewState extends State<WatchLaterHorizontalView> {
+  final PageController _pageController = PageController(viewportFraction: 0.7);
+  double _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          double scale = (_currentPage - index).abs().clamp(0.0, 1.0);
+          double size = 1 - scale * 0.2; // تصغير حسب المسافة
+
+          return Transform.scale(
+            scale: size,
+            child: const CustomCard(
+              imageUrl: AppImages.movieBackground,
+              title: 'Avengers: Endgame',
+              rating: 0.9,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
