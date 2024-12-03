25580cb72b63636372a59d403eeb45ffad8212a3
Youssef-Khorshed <youssefmohamedeslayed2000@gmail.com>
2024-12-03 19:04:31 +0200

    all mine

--- lib/features/menu_screens/presentation/widgets/movie_download_widget/movie_download_card.dart
+++ lib/features/menu_screens/presentation/widgets/movie_download_widget/movie_download_card.dart
@@ -13,21 +13,20 @@ class MovieDownloadCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 212,
-
-      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       decoration: BoxDecoration(
-
         borderRadius: BorderRadius.circular(20),
-        gradient:ThemeCubit.get(context).themeModeState == ThemeModeState.dark? LinearGradient(
-          colors: [AppColors.black2, AppColors.gray1],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ): const
-        LinearGradient(
-          colors: [AppColors.purpleblue, AppColors.white],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
+        gradient: ThemeCubit.get(context).themeModeState == ThemeModeState.dark
+            ? const LinearGradient(
+                colors: [AppColors.black2, AppColors.gray1],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              )
+            : const LinearGradient(
+                colors: [AppColors.purpleblue, AppColors.white],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.1),
@@ -42,13 +41,11 @@ class MovieDownloadCard extends StatelessWidget {
           Align(
             alignment: Alignment.centerLeft,
             child: ClipRRect(
-
               borderRadius: BorderRadius.circular(20),
               child: Image.asset(
-                height:160 ,
+                height: 160,
                 width: 95,
                 AppImages.movieBackground,
-
                 fit: BoxFit.fill,
               ),
             ),
@@ -61,10 +58,7 @@ class MovieDownloadCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                    'Joker',
-                    style: AppTextStyle.style20WhiteW400
-                ),
+                Text('Joker', style: AppTextStyle.style20WhiteW400),
                 const SizedBox(height: 8),
                 Text(
                   '2022, Horror',
@@ -78,39 +72,49 @@ class MovieDownloadCard extends StatelessWidget {
                       width: 99,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        gradient:ThemeCubit.get(context).themeModeState == ThemeModeState.dark? LinearGradient(
-                          colors: [AppColors.purple2, AppColors.gray1],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ): const
-                        LinearGradient(
-                          colors: [AppColors.purpleblue, AppColors.white],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
+                        gradient: ThemeCubit.get(context).themeModeState ==
+                                ThemeModeState.dark
+                            ? const LinearGradient(
+                                colors: [AppColors.purple2, AppColors.gray1],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              )
+                            : const LinearGradient(
+                                colors: [AppColors.purpleblue, AppColors.white],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
                       ),
-                      child: Center(child: Text('Watch',style: AppTextStyle.style14WhiteW400,)),
+                      child: Center(
+                          child: Text(
+                        'Watch',
+                        style: AppTextStyle.style14WhiteW400,
+                      )),
                     ),
                     const SizedBox(width: 16),
                     Container(
-                      height: 40,
-                      width: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient:ThemeCubit.get(context).themeModeState == ThemeModeState.dark? LinearGradient(
-                          colors: [AppColors.purple2, AppColors.gray1],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ): const
-                        LinearGradient(
-                          colors: [AppColors.menuPurpleColor, AppColors.darkpurple],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
+                        height: 40,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: ThemeCubit.get(context).themeModeState ==
+                                  ThemeModeState.dark
+                              ? const LinearGradient(
+                                  colors: [AppColors.purple2, AppColors.gray1],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                )
+                              : const LinearGradient(
+                                  colors: [
+                                    AppColors.purple,
+                                    AppColors.darkpurple
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
                         ),
-                      )
-                      ,child:  Icon(Icons.delete, color: AppColors.white)),
-
-
+                        child:
+                            const Icon(Icons.delete, color: AppColors.white)),
                   ],
                 ),
               ],
